4302c3a9c1c46f2367374102eab7ab481f91dc47
PPrzebieda <przemyslaw.przebieda@gmail.com>
2023-04-28 10:01:07 +0200

    Links fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,16 @@ import hamburger from "../images/hamburger_menu.png";
 export default function Navbar() {
   return (
     <div className="navbar-container">
-      <a href="home">
+      <a href="#">
         <img src={logo} alt="site logo" />
       </a>
       <div className="links">
-        <a href="home">HOME</a>
-        <a href="about">ABOUT</a>
-        <a href="ourcars">OUR CARS</a>
-        <a href="opinions">OPINIONS</a>
-        <a href="ourteam">OUR TEAM</a>
-        <a href="contact">CONTACT</a>
+        <a href="#">HOME</a>
+        <a href="#">ABOUT</a>
+        <a href="#">OUR CARS</a>
+        <a href="#s">OPINIONS</a>
+        <a href="#">OUR TEAM</a>
+        <a href="#">CONTACT</a>
       </div>
       <button className="btn login">Log in</button>
       <button className="btn register">Register</button>
